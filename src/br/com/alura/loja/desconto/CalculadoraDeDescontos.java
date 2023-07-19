29d277cd412e384e1d5d5cf2d36cb9ef4dcad1cf
Daniel Adriano <dmoura.adriano@gmail.com>
2023-07-19 15:02:44 -0300

    updates

--- src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -1,18 +1,15 @@
 package br.com.alura.loja.desconto;
-
 import br.com.alura.loja.orcamento.Orcamento;
-
 import java.math.BigDecimal;
+
 
 public class CalculadoraDeDescontos {
 
     public BigDecimal calcular(Orcamento orcamento){
-        if (orcamento.getQuantidadeDeItens() > 5){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        if(orcamento.getValor().compareTo(new BigDecimal("500")) > 0){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        return BigDecimal.ZERO;
+        Desconto desconto = new DescontoParaOrcamentoMaisDeCincoItens(
+                new DescontoParaOrcamentoMaiorQueQuinentos(
+                        new SemDesconto()));
+
+        return desconto.calcular(orcamento);
     }
 }
