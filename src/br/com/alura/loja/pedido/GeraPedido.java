2e444fe77666170f2c1549a321a5b032374ac00b
Daniel Adriano <dmoura.adriano@gmail.com>
2023-07-20 15:25:39 -0300

    updates

--- src/br/com/alura/loja/pedido/GeraPedido.java
+++ src/br/com/alura/loja/pedido/GeraPedido.java
@@ -17,13 +17,15 @@ public class GeraPedido {
         this.quantidadeItens = quantidadeItens;
     }
 
-    public void executa(){
-        Orcamento orcamento = new Orcamento(this.valorOrcamento, this.quantidadeItens);
-
-        Pedido pedido = new Pedido(cliente, LocalDateTime.now(), orcamento);
-
-        System.out.println("Salvar pedido no Banco de Dados");
-        System.out.println("Enviar e-mail com dados do novo pedido");
+    public String getCliente() {
+        return cliente;
     }
 
+    public BigDecimal getValorOrcamento() {
+        return valorOrcamento;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
+    }
 }
