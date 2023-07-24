9f130c3cee1a7db50e981bd202c8a1c7f47b8926
Daniel Adriano <dmoura.adriano@gmail.com>
2023-07-24 15:05:55 -0300

    Aplicando Composite e Facade

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -2,8 +2,9 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
-import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
-import br.com.alura.loja.pedido.acao.SalvarPedidoBD;
+import br.com.alura.loja.pedido.acoes.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acoes.LogDePedido;
+import br.com.alura.loja.pedido.acoes.SalvarPedidoBD;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -11,12 +12,15 @@ import java.util.Arrays;
 public class TestesPedidos {
     public static void main(String[] args) {
         String cliente = "Daniel";
-        BigDecimal valorOrcamento = new BigDecimal("300");
-        int quantidadeItens = Integer.parseInt("2");
+        BigDecimal valorOrcamento = new BigDecimal("745.99");
+        int quantidadeItens = 3;
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(
-                Arrays.asList(new SalvarPedidoBD(), new EnviarEmailPedido()));
-        handler.execute(gerador);
+        GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(
+                        new SalvarPedidoBD(),
+                        new EnviarEmailPedido(),
+                        new LogDePedido()));
+
+        handler.executar(gerador);
     }
 }
--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,26 +1,26 @@
 package br.com.alura.loja.pedido;
-
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
-import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acoes.AcaoAposGerarPedido;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class GeraPedidoHandler {
 
-    private List<AcaoAposGerarPedido> acoes;
+    private List<AcaoAposGerarPedido> acoesAposGerarPedido;
 
-    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
-        this.acoes = acoes;
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoesAposGerarPedido) {
+        this.acoesAposGerarPedido = acoesAposGerarPedido;
     }
 
-    public void execute(GeraPedido dados) {
+    public void executar(GeraPedido geraPedido) {
         Orcamento orcamento = new Orcamento();
+        orcamento.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
+        Pedido pedido = new Pedido(geraPedido.getCliente(), LocalDateTime.now(), orcamento);
 
-        Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
-
-        acoes.forEach(a -> a.executarAcao(pedido));
-
+        this.acoesAposGerarPedido.forEach(a -> a.executarAcao(pedido));
     }
 
 }
